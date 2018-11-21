87edf20724ca74b08cf1537c849bedc0c6e0aa01
G4tlan <1043882@students.wits.ac.za>
2018-11-21 23:51:49 +0200

    Facilities page

--- app/src/main/java/com/programminghub/upapp/Login.java
+++ app/src/main/java/com/programminghub/upapp/Login.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     TextView txtLogin ;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         txtLogin = findViewById(R.id.txtLogIn);
+        login = findViewById(R.id.loginButton);
 
         txtLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +27,14 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),SignIn.class));
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), Facilities.class));
+            }
+        });
+
     }
 }
